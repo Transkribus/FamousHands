ba5921a464ffaf87ba9a7a8d06b779c433622775
albertgreinoecker <albert.greinoecker@gmail.com>
2017-10-30 13:18:17 +0100

    minor  modifications

--- fh2/fh/tools/EntryUploader.py
+++ fh2/fh/tools/EntryUploader.py
@@ -1,5 +1,3 @@
-import csv
-import os
 
 '''
 Created on Aug 4, 2017
@@ -14,41 +12,46 @@ import fh.models as m
 import datetime
 from shutil import copyfile
 import os
+import csv
+
 
 class EntryUploader:
 
-    def setEntry(self, q, label):
+    def setEntry(self, qid, label):
         wi = w.WikiGet()
         #wi.GetEntry("Q7346")
-        entry = wi.GetEntry(q, label)
+        
+        if not m.ENTRY.objects.filter(wiki_link=qid).exists():
+            entry = wi.GetEntry(qid, label)
     
     
     def upload(self):
-        with open('/home/albert/tmp/fh/Famous Hands/tab2.csv', 'rt') as csvfile:
-            f = csv.reader(csvfile, delimiter=',', quotechar='"')
-            q_label = {};
+        with open('/home/albert/tmp/fh/Famous Hands/q_folder.csv', 'rt') as csvfile:
+            f = csv.reader(csvfile, delimiter='\t', quotechar='"')
+            #q_label = {};
             next(f) #ignore header
             for row in f:
-                if  len(row[2]) > 0 and row[2].startswith('Q'):
-                    print  (row[2]);
-                    q_label[ row[2]] = row[0]
-                    self.setEntry(row[2], row[0])
-                    
+                #if  len(row[2]) > 0 and row[2].startswith('Q'):
+                    #print  (row[2]);
+                    #q_label[ row[2]] = row[0]
+                    self.setEntry(row[0], row[1])
+    
 
+    
     def uploadHandwriting(self):
         with open('/home/albert/tmp/fh/Famous Hands/q_folder.csv', 'rt') as csvfile:
             f = csv.reader(csvfile, delimiter='\t', quotechar='"')
             next(f) #ignore header
             for row in f:
                 print (row[0])
-                new_dir = '/home/albert/Dropbox/my/workspaces/liclipse/fh2/fh/static/fh/img/upload/' + row[2];
+                new_dir = '/home/albert/Dropbox/my/workspaces/liclipse/fh2/fh/static/fh/img/upload/' + row[0];
                 if not os.path.exists(new_dir):
                     os.makedirs(new_dir)
                 i = 0
                 for img in os.listdir('/home/albert/tmp/fh/Famous Hands/' + row[2]):
-                    img_p = row[2] + '/' + img;
+                    img_p = row[0].strip() + '/' + img
                     print(row[3])
                     en = m.ENTRY.objects.filter(wiki_link=row[0]).first();                    
-                    m.HANDWRITTENIMAGE.objects.create(entry = en, title = row[2]  + "_" + str(i), description = row[2]  + "_" + str(i), date = datetime.datetime.now(), link = img_p)
-                    #copyfile('/home/albert/tmp/fh/Famous Hands/' + img_p, '/home/albert/Dropbox/my/workspaces/liclipse/fh2/fh/static/fh/img/upload/' + img_p)
+                    m.HANDWRITTENIMAGE.objects.create(entry = en, title = row[2].strip()  + "_" + str(i), description = row[2]  + "_" + str(i), date = datetime.datetime.now(), link = img_p)
+                    #copyfile('/home/albert/tmp/fh/Famous Hands/' + row[2].strip()  + '/' + img, '/home/albert/Dropbox/my/workspaces/liclipse/fh2/fh/static/fh/img/upload/' + img_p)
                     i += 1
